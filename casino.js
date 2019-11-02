76361b228f9398eb3a49d41ee6d15f7b28a56c5b
Uberknorr <aknorr222@gmail.com>
2019-11-02 10:01:14 -0600

    choice_handler

--- casino.js
+++ casino.js
@@ -6,10 +6,28 @@ function intro(){
 
 
 function choice_handler(input_sel){
-    if(input_sel ===1 || input_sel.toLowerCase === "black jack"){
-        console.log("black jack")
-    }
+  alert(input_sel)
+  if(input_sel ===1 || input_sel.toLowerCase === "black jack"){
+    alert("black jack")
+  }else if(input_sel === 2 || input_sel.toLowerCase === "slot machine"){
+    alert("slot machine")
+  }else if(input_sel === 3 || input_sel.toLowerCase === "roulette"){
+    DeviceAcceleration("roulette")
+  }
+  else{
+    alert("Im Sorry That's Not A Game We Have, Please Try Again.")
+    let try_again = prompt(
+        "Casino Royal Game List\n"+
+        "Pleas Enter The Gamer Number Or\n Game Name To Select Game"+
+        "*1.Black Jack*\n"+
+        "*2.Slot Machine*\n"+
+        "*3.Roulette*\n"+
+        "***************************\n"+
+        "What Game Would You Like To Play?")
+    choice_handler(menu_option);
+  }
 }
+
 
 function menu(){
     let quit = false;
@@ -22,11 +40,13 @@ function menu(){
 
       let menu_option = prompt(
         "Casino Royal Game List\n"+
-        "*1.black jack*\n"+
+        "Pleas Enter The Gamer Number Or\n Game Name To Select Game"+
+        "*1.Black Jack*\n"+
         "*2.Slot Machine*\n"+
         "*3.Roulette*\n"+
         "***************************\n"+
         "What Game Would You Like To Play?")
+      console.log(menu_option)
       choice_handler(menu_option);
       quit = true;
     }
