7022bf6fc94ca8c9f772df43721a1d0e6f04b38c
Uberknorr <aknorr222@gmail.com>
2019-11-02 09:31:25 -0600

    This is the first commit

---
+++ casino.js
@@ -0,0 +1,17 @@
+async function intro(){
+    console.log("Welcome to Consino Royal");
+    console.log("This is a test game made by Andrew Knorr");
+    return new Promise(resolve => {resolve('resolved'); }, 2000);
+}
+
+
+function menu(){
+    let quit = false;
+    let intro_ran = false;
+
+    while(quit === false){
+        if (intro_ran === false){
+
+        }
+    }
+}
