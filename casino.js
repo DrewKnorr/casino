7355a28cfec3ed79ff11206d9bbb0bfb61e0743f
Uberknorr <aknorr222@gmail.com>
2019-11-02 09:55:22 -0600

    Alert/Prompt

--- casino.js
+++ casino.js
@@ -1,9 +1,15 @@
-async function intro(){
-    console.log("Welcome to Consino Royal");
-    console.log("This is a test game made by Andrew Knorr");
-    return new Promise(resolve => {resolve('resolved'); }, 2000);
+function intro(){
+    alert("Welcome to Casino Royal\nThis is a test game made by Andrew Knorr");
+    return true
 }
 
+
+
+function choice_handler(input_sel){
+    if(input_sel ===1 || input_sel.toLowerCase === "black jack"){
+        console.log("black jack")
+    }
+}
 
 function menu(){
     let quit = false;
@@ -11,7 +17,19 @@ function menu(){
 
     while(quit === false){
         if (intro_ran === false){
-
+          intro_ran=intro();
         }
+
+      let menu_option = prompt(
+        "Casino Royal Game List\n"+
+        "*1.black jack*\n"+
+        "*2.Slot Machine*\n"+
+        "*3.Roulette*\n"+
+        "***************************\n"+
+        "What Game Would You Like To Play?")
+      choice_handler(menu_option);
+      quit = true;
     }
 }
+
+menu();
